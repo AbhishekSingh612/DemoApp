e345b61e72b22d3cae7d8082f1b793d7b26576da
AbhishekSingh612 <67855380+AbhishekSingh612@users.noreply.github.com>
2025-03-10 03:20:14 +0530

    - Added sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,41 @@
 <script lang="ts">
-	import '../app.css';
+    import '../app.css';
     import GalleryVerticalEnd from "lucide-svelte/icons/gallery-vertical-end";
-    import { ModeWatcher } from "mode-watcher";
+    import {ModeWatcher} from "mode-watcher";
     import ThemeToggle from "$lib/components/ui/theme-toggle/theme-toggle.svelte";
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import AppSidebar from "$lib//components/ui/app-sidebar.svelte";
     import {APP_NAME} from "$lib";
-	let { children } = $props();
+
+    let {children, data} = $props();
+
+    // Check if user exists in the data
+    let user = $derived(data?.user);
+    let isLoggedIn = $derived(!!user);
 </script>
-<ModeWatcher />
-<div class="grid min-h-svh">
-    <div class="flex flex-col gap-4 p-6">
-        <div class="flex justify-between items-center gap-2">
+<ModeWatcher/>
+<Sidebar.Provider>
+    <Sidebar.Inset>
+        <header class="flex justify-between h-16 shrink-0 items-center gap-2 px-4">
             <a href="#" class="flex items-center gap-2 font-medium">
                 <div class="flex h-6 w-6 items-center justify-center rounded-md">
-                    <GalleryVerticalEnd />
+                    <GalleryVerticalEnd/>
                 </div>
                 {APP_NAME}
             </a>
-            <ThemeToggle />
-        </div>
-        <div class="flex flex-1 items-center justify-center">
+            <div class="flex justify-between items-center gap-2">
+                <ThemeToggle/>
+                {#if isLoggedIn}
+                    <Sidebar.Trigger class="-ml-1"/>
+                {/if}
+            </div>
+        </header>
+        <!--Main Body-->
+        <div class="flex flex-1 flex-col gap-4 p-4 items-center justify-center">
             {@render children()}
         </div>
-    </div>
-</div>
-
-
+    </Sidebar.Inset>
+    {#if isLoggedIn}
+        <AppSidebar/>
+    {/if}
+</Sidebar.Provider>
